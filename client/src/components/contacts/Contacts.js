7e193a59169cf487af33f6ee7218b2270166debc
Spencer LaPray <sblapray@yahoo.com>
2019-06-14 22:12:37 -0600

    contact filter is working and displaying in the UI

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -5,12 +5,20 @@ import ContactContext from "../../context/contact/contactContext";
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
 
-  const { contacts } = contactContext;
+  const { contacts, filtered } = contactContext;
+
+  if (contacts.length === 0) {
+    return <h4>Please Add a Contact</h4>;
+  }
   return (
     <Fragment>
-      {contacts.map(contact => (
-        <ContactItem key={contact.id} contact={contact} />
-      ))}
+      {filtered !== null
+        ? filtered.map(contact => (
+            <ContactItem key={contact.id} contact={contact} />
+          ))
+        : contacts.map(contact => (
+            <ContactItem key={contact.id} contact={contact} />
+          ))}
     </Fragment>
   );
 };
