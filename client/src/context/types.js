c5b7c5f504d86a0609032c0194d8a93ef02623c2
Spencer LaPray <sblapray@yahoo.com>
2019-06-15 13:33:56 -0600

    Register, Login and Alert complete

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -6,6 +6,7 @@ export const UPDATE_CONTACT = "UPDATE_CONTACT";
 export const FILTER_CONTACTS = "FILTER_CONTACTS";
 export const CLEAR_FILTER = "CLEAR_FILTER";
 export const SET_ALERT = "SET_ALERT";
+export const REMOVE_ALERT = "REMOVE_ALERT";
 export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
 export const REGISTER_FAIL = "REGISTER_FAIL";
 export const USER_LOADED = "USER_LOADED";
