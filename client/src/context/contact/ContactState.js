61e2f63d73245199efdc0635999d18c35ad8bd2e
Spencer LaPray <sblapray@yahoo.com>
2019-06-15 15:07:46 -0600

    completed application with full CRUD

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -64,11 +64,45 @@ const ContactState = props => {
   };
 
   // Delete Contact
-  const deleteContact = id => {
-    dispatch({
-      type: DELETE_CONTACT,
-      payload: id
-    });
+  const deleteContact = async id => {
+    try {
+      await axios.delete(`/api/contacts/${id}`);
+      dispatch({
+        type: DELETE_CONTACT,
+        payload: id
+      });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.msg
+      });
+    }
+  };
+
+  // Update Contact
+  const updateContact = async contact => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+      dispatch({
+        type: UPDATE_CONTACT,
+        payload: res.data
+      });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.msg
+      });
+    }
   };
 
   // Clear Contacts
@@ -89,14 +123,6 @@ const ContactState = props => {
   const clearCurrent = () => {
     dispatch({
       type: CLEAR_CURRENT
-    });
-  };
-
-  // Update Contact
-  const updateContact = contact => {
-    dispatch({
-      type: UPDATE_CONTACT,
-      payload: contact
     });
   };
 
--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -23,21 +23,21 @@ export default (state, action) => {
     case ADD_CONTACT:
       return {
         ...state,
-        contacts: [...state.contacts, payload],
+        contacts: [payload, ...state.contacts],
         loading: false
       };
     case UPDATE_CONTACT:
       return {
         ...state,
         contacts: state.contacts.map(contact =>
-          contact.id === payload.id ? payload : contact
+          contact._id === payload._id ? payload : contact
         ),
         loading: false
       };
     case DELETE_CONTACT:
       return {
         ...state,
-        contacts: state.contacts.filter(contact => contact.id !== payload),
+        contacts: state.contacts.filter(contact => contact._id !== payload),
         loading: false
       };
     case CLEAR_CONTACTS:
